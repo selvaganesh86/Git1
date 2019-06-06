473282cbe2710aa9321595da386f8442f3f87acb
selvaganesh86 <51355158+selvaganesh86@users.noreply.github.com>
2019-06-06 14:07:54 +0530

    Update foutlier.py

--- foutlier.py
+++ foutlier.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import numpy as np
+from scipy inmport stats
+from pyod.models.knn import KNN
+
+def scm(bu_var, word_var, te_amt_var, scm_org_data, sheet_id)
+  # create new filtered data frame based on parameters received
+  
